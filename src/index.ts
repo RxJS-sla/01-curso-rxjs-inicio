9cdb487a84d1b72fc5769a786df16ed27ff615bb
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-01-07 20:50:01 -0500

    :zap: primer observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
+import { Observable } from "rxjs";
 
+// const observable$ = Observable.create(); = const observable$ = new Observable(); mismo resultado
+const observable$ = new Observable<string>(subscriber => {
+    subscriber.next("Hola");
+    subscriber.next("Mundo");
 
-console.log('Hola CAO!');
+    // // Forzar un error
+    // const a = undefined;
+    // a.nombre = "Fernando";
 
+    //aca se completa el observable
+    subscriber.complete();
 
+    subscriber.next("Hola");
+    subscriber.next("Mundo");
+});
 
-
-
-
-
+observable$.subscribe(console.log);
