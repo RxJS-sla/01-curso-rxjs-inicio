bb6840a1f69ed6e9fd5f73207f23b6f32cdd6cdb
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-01-07 21:14:15 -0500

    :zap: observable y subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import { Observable } from "rxjs";
+import { Observable , Observer} from "rxjs";
+
+
+
+const observer: Observer<string> = {
+    next: value => console.log('next: ', value),
+    error: error => console.warn('error: ', error),
+    complete: () => console.info('complete')
+};
 
 // const observable$ = Observable.create(); = const observable$ = new Observable(); mismo resultado
 const observable$ = new Observable<string>(subscriber => {
     subscriber.next("Hola");
     subscriber.next("Mundo");
 
-    // // Forzar un error
+    // // Forzar un error -> si encuentra un error en el observable, no se ejecuta el complete
     // const a = undefined;
-    // a.nombre = "Fernando";
+    // a.nombre = "Santiago";
 
     //aca se completa el observable
     subscriber.complete();
@@ -16,4 +24,11 @@ const observable$ = new Observable<string>(subscriber => {
     subscriber.next("Mundo");
 });
 
-observable$.subscribe(console.log);
+// observable$.subscribe(res=>console.log(res));
+// observable$.subscribe(console.log);
+// observable$.subscribe({
+//     next: next => console.log('Next: ', next),
+//     error: error => console.warn('Error: ', error),
+//     complete: () => console.log('COMPLETE')
+// });
+observable$.subscribe(observer);
