5158e847e7a0828a7ca3a68e7a5b5f6e76890143
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-01-09 19:29:58 -0500

    :zap: 04-of

--- src/observables/04-of.ts
+++ src/observables/04-of.ts
@@ -1,13 +1,13 @@
 import { of } from "rxjs";
 
 
- const observable$ = of(1,2,3,4,5,6);
+const observable$ = of(1, 2, 3, 4, 5, 6);
 // const observable$ = of(...[1,2,3,4,5,6]);
 // const observable$ = of([1,2],{a:1,b:2},true, Promise.resolve(true));
 
 console.log('inicio del observable')
 observable$.subscribe({
     next: value => console.log('next: ', value),
-    complete:()=> console.log('complete sequence'),
+    complete: () => console.log('complete sequence'),
 })
 console.log('fin del observable')
