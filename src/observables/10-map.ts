ebb0483a2d259630bf193a04c075e47dfd938acc
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-02-19 22:34:54 -0500

    :zap: map

--- src/observables/10-map.ts
+++ src/observables/10-map.ts
@@ -16,6 +16,12 @@ const keyUpPluck$ = keyUp$.pipe(
     map(x => x.key)
 )
 
+
+const keyUpMapTo$ = keyUp$.pipe(
+    map(()=> 'tecla presionada')
+)
+
 keyUp$.subscribe(code => console.log('all', code))
 keyCode$.subscribe(code => console.log('map', code))
 keyUpPluck$.subscribe(code => console.log('pluck', code))
+keyUpMapTo$.subscribe(code => console.log('mapTo', code))
