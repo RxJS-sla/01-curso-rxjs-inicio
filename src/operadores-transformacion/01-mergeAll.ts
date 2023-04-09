d680b71b2af7d9b57c350872e80acd30280ac62d
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-04-09 14:48:22 -0500

    :zap: mergeAll

---
+++ src/operadores-transformacion/01-mergeAll.ts
@@ -0,0 +1,63 @@
+import { fromEvent, Observable } from 'rxjs';
+import { debounceTime, map, mergeAll } from 'rxjs/operators';
+
+import { ajax } from 'rxjs/ajax';
+
+import { GithubUser, GithubUsersResp } from '../interfaces';
+
+
+
+// Referencias
+const body = document.querySelector('body');
+const textInput = document.createElement('input');
+const orderList = document.createElement('ol');
+body.append(textInput, orderList);
+
+// Helpers
+const mostrarUsuarios = (usuarios: GithubUser[]) => {
+
+    console.log(usuarios);
+    orderList.innerHTML = '';
+
+    for (const usuario of usuarios) {
+
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        img.src = usuario.avatar_url;
+
+        const anchor = document.createElement('a');
+        anchor.href = usuario.html_url;
+        anchor.text = 'Ver página';
+        anchor.target = '_blank';
+
+        li.append(img);
+        li.append(usuario.login + ' ');
+        li.append(anchor);
+
+        orderList.append(li);
+    }
+
+}
+
+//mergeall-> terminar observables
+
+// Streams
+const input$ = fromEvent<KeyboardEvent>(textInput, 'keyup');
+
+// input$.pipe(
+//     debounceTime<KeyboardEvent>(500),
+//     map<KeyboardEvent, string>(({ target }) => (target as HTMLInputElement).value),
+//     map<string, Observable<GithubUsersResp>>(texto => ajax.getJSON(`https://api.github.com/search/users?q=${texto}`)),
+//     mergeAll<Observable<GithubUsersResp>>(),
+//     map<GithubUsersResp, GithubUser[]>(x => x.items)
+// ).subscribe(mostrarUsuarios);
+
+
+input$.pipe(
+    debounceTime<KeyboardEvent>(500),
+    map(({ target }) => (target as HTMLInputElement).value),
+    map<string, Observable<GithubUsersResp>>(texto => ajax.getJSON(`https://api.github.com/search/users?q=${texto}`)),
+    mergeAll(),
+    map(x => x.items)
+).subscribe(mostrarUsuarios);
+
