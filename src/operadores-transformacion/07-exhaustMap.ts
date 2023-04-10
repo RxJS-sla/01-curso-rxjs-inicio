7f745233645d54efd1275c4d004fec2bcab3159a
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2023-04-09 19:28:02 -0500

    :zap: exhaustMap

--- src/operadores-transformacion/07-exhaustMap.ts
+++ src/operadores-transformacion/07-exhaustMap.ts
@@ -1,7 +1,7 @@
 import { interval, fromEvent } from 'rxjs';
 import { take, exhaustMap } from 'rxjs/operators';
 
-
+// se ejecuta la siguiente hasta que acabe el anterior, no sigue en la cola hasta que finalice
 
 const interval$ = interval(500).pipe( take(3) );
 const click$    = fromEvent( document, 'click' );
